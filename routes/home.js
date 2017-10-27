47f6175a4005efc97fd9581792775fcb6271283f
Sean McNeeley <sean21mcn@gmail.com>
2017-10-26 20:35:22 -0400

    uninstalled and reinstalled all node modules, edited expression of var models in routes/home.js, added a termporary logout page as well as a profilesample page in views

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
+var models = require('../db');
+
 
 module.exports = function(passport) {
 	router.get('/home', require('connect-ensure-login').ensureLoggedIn(), function(req, res){
@@ -28,8 +30,43 @@ var requestHandler = multer({ storage: myStorage })
 
 router.post('/create', requestHandler.single('nameofField'),
     function(req, res, next) {
+			models.post.findAll({
+				include: [models.posts]
 
+			}).then(function(allPosts) {
+				res.render('home', {
+
+				});
+				models.likes.findAll({
+					include: [models.posts]
+
+				}).then(function(likes) {
+					res.render('home', {
+
+					});
+			})
     }
-
-
 );
+
+
+// //Check if Genre with same name already exists
+// Genre.findOne({ 'name': req.body.name })
+//     .exec( function(err, found_genre) {
+//     console.log('found_genre: '+found_genre)
+//     if (err) { return next(err); }
+//
+//         if (found_genre) {
+//             //Genre exists, redirect to its detail page
+//             res.redirect(found_genre.url);
+//             }
+//         else {
+//             genre.save(function (err) {
+//                 if (err) { return next(err); }
+//                     //Genre saved. Redirect to genre detail page
+//                     res.redirect(genre.url);
+//                 });
+//         }
+//
+// });
+
+// eg
