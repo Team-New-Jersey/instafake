38f6bdfb02783fd5fbbd49e81dd589637ab69428
Sean McNeeley <sean21mcn@gmail.com>
2017-10-30 23:16:01 -0400

    simplified likes, comments and posts in views/home, changed log in info in routes/home

--- routes/home.js
+++ routes/home.js
@@ -5,8 +5,8 @@ require('./login');
 
 var pool = new pg.Pool({
 	host: 'localhost',
-	user: 'postgres',
-	port: 1234,
+	user: 'Nemiliox',
+	port: 5432,
 	database: 'instabase',
 	password: ''
 });
@@ -16,34 +16,34 @@ var User = require('../db').users;
 router.get('/', function(req, res, next) {
 	var lgdUserId = req.cookies['userid'];
     var lgdUsername = req.cookies['username'];
-	
+
 	pool.connect(function(err, client, done) {
 
-		if(err) throw err; 
-		client.query('SELECT ARRAY (SELECT id FROM users); SELECT ARRAY (SELECT username FROM users); SELECT ARRAY (SELECT id FROM posts); SELECT ARRAY (SELECT user_id FROM posts); SELECT ARRAY (SELECT description FROM posts); SELECT ARRAY (SELECT id FROM likes); SELECT * FROM likes WHERE post_id IN (SELECT post_id FROM likes GROUP BY post_id having count(*) > 2); SELECT ARRAY (SELECT user_id FROM likes); SELECT ARRAY (SELECT post_id FROM likes); SELECT ARRAY (SELECT id FROM comments); SELECT ARRAY (SELECT comment FROM comments); SELECT ARRAY (SELECT user_id FROM comments); SELECT ARRAY (SELECT post_id FROM comments)', 
+		if(err) throw err;
+		client.query('SELECT ARRAY (SELECT id FROM users); SELECT ARRAY (SELECT username FROM users); SELECT ARRAY (SELECT id FROM posts); SELECT ARRAY (SELECT user_id FROM posts); SELECT ARRAY (SELECT description FROM posts); SELECT ARRAY (SELECT id FROM likes); SELECT * FROM likes WHERE post_id IN (SELECT post_id FROM likes GROUP BY post_id having count(*) > 2); SELECT ARRAY (SELECT user_id FROM likes); SELECT ARRAY (SELECT post_id FROM likes); SELECT ARRAY (SELECT id FROM comments); SELECT ARRAY (SELECT comment FROM comments); SELECT ARRAY (SELECT user_id FROM comments); SELECT ARRAY (SELECT post_id FROM comments)',
 			(err, result) => {
  			if (err) {
    	 			throw err
  			}
 
-   	 		
-			var userIds = result[0].rows[0].array; 
+
+			var userIds = result[0].rows[0].array;
   			var usernames = result[1].rows[0].array;
   			var postIds = result[2].rows[0].array;
-  			var postUserIds = result[3].rows[0].array; 
-  			var postDescriptions = result[4].rows[0].array; 
+  			var postUserIds = result[3].rows[0].array;
+  			var postDescriptions = result[4].rows[0].array;
   			var likeIds = result[5].rows[0].array;
   			var likesPerPost = result[6].rows;
-  			var likeUserIds = result[7].rows[0].array; 
-  			var likePostIds = result[8].rows[0].array; 
-  			var commentIds = result[9].rows[0].array; 
-  			var commentBodies = result[10].rows[0].array; 
+  			var likeUserIds = result[7].rows[0].array;
+  			var likePostIds = result[8].rows[0].array;
+  			var commentIds = result[9].rows[0].array;
+  			var commentBodies = result[10].rows[0].array;
   			var commentUserIds = result[11].rows[0].array;
   			var commentPostIds = result[12].rows[0].array;
-  					
+
 			// done();
 			// pool.end();
-			res.render('home', { 
+			res.render('home', {
 		    	lgdUserId: lgdUserId,
 		    	lgdUsername: lgdUsername,
 		    	userIds: userIds,
