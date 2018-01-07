7f34df969ae7ba9af2f347883b96353fd757180e
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-06 19:11:42 -0700

    development changed to production in config

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
+var env = process.env.NODE_ENV;
 
 // Load DB config from config file
 var config = require(path.join(__dirname, 'config', 'config.js'));
--- server.babel.js
+++ server.babel.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var passport = require('passport');
 var connection = require('./sql.js');
+var env = process.env.NODE_ENV;
 connection.sync();
 
 var app = express();
