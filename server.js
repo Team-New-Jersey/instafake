a1acab5f54be40686c370f0e06cacef6421445eb
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-21 18:26:32 -0700

    Deploy attempt

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ require('babel-register');
 
 var app = require('./server.babel');
 var Sequelize = require('sequelize');
-var sequelize = new Sequelize(process.env.CONN_STRING);
+var sequelize = new Sequelize('process.env.CONN_STRING');
 
 module.exports = app;
