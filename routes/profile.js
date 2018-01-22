04070eecdb3612391120241d226cf7a086b20cd5
Raymond Ajax Metrulis <raymondajaxmetrulis@gmail.com>
2018-01-21 23:21:01 -0700

    commit to push, gitignore .env

--- routes/profile.js
+++ routes/profile.js
@@ -8,12 +8,15 @@ var Posts = require('../db').posts;
 var Comment = require('../db').comments;
 var iLike = require('../db').likes;
 
-var pool = new pg.Pool({
-    user: 'pvswpjtuimldnh',
-    password: 'fad3cd7f7b89d6098d063a417646cea42b727afd3fb1d48953c65ed88b58afee',
-    host: 'ec2-54-235-73-241.compute-1.amazonaws.com',
+require('dotenv').config();
+
+
+var pool = new pg.Pool ({
+	user: process.env.HEROKU_DB_USER,
+    password: process.env.HEROKU_DB_PW,
+    host: process.env.HEROKU_DB_HOST,
     port: 5432,
-    database: 'd9ev6uhm5gsgaj'
+    database: process.env.HEROKU_DB_NAME
 });
 
 var lgdUserId;
